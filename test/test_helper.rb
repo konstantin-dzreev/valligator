93954b1f9371b43cf582889eb1e4ef837158da13
Konstantin Dzreev <k.dzreyev@gmail.com>
2017-11-13 23:14:16 -0800

    Do not use minitest/reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,2 @@
 require 'minitest/autorun'
-require 'minitest/reporters'
 require_relative '../lib/valligator'
-
-Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new({ color: true })]
